8e877f4f44be6dd8e7cccc4312fe7abeb8136e6f
chekotisrikanth <chekotisrikanth@gmail.com>
2024-12-24 09:21:52 +0530

    update test file

--- pyoauthbridge/test_connection.py
+++ pyoauthbridge/test_connection.py
@@ -1,32 +1,37 @@
 from connect2 import Connect
 from flask import Flask, request, redirect
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get credentials from environment variables
+app_id = os.getenv('APP_ID')
+app_secret = os.getenv('APP_SECRET')
+redirect_url = os.getenv('REDIRECT_URL')
+base_url = os.getenv('BASE_URL')
+login_id = os.getenv('LOGIN_ID')
+login_pwd = os.getenv('LOGIN_PASSWORD')
+totp_secret = os.getenv('TOTP_SECRET')
+auto_login = os.getenv('AUTO_LOGIN').lower() == 'true'
 
 
-# Replace with your actual credentials
-app_id = "SAS-CLIENT1"
-app_secret = "your client_secret"
-redirect_url = "http://127.0.0.1/"
-base_url = "https://api.stocko.in"
-client_id = "your client_id"
+conn = Connect(app_id, app_secret, redirect_url, base_url,login_id,login_pwd,totp_secret)
 
-#access_token = "YOUR_ACCESS_TOKEN"  # Or obtain it using conn.get_access_token()
-
-conn = Connect(app_id, app_secret, redirect_url, base_url)
-
-access_token = conn.get_access_token()
+access_token = conn.get_access_token(auto_login)
 
 print('access_token ',access_token)
 
-#access_token = "NQOGiY16aFrKqRque8HMg5TVCGilCaiSnWDMITIctQk.VIOU6sRIYAsxdPSJqRkzl4b9x32JAqzpLAJ0_4UDxSw"
+
 if access_token:
     conn.set_access_token(access_token)
 
 payload = {
-    'client_id': client_id
+    'client_id': login_id  # Example payload
 }
 profile_res = conn.fetch_profile(payload)
 print(profile_res)
 
-
-
-
+script_searchResp = conn.search_scrip({'key': 'NIFTY 50'})
+print(script_searchResp)
